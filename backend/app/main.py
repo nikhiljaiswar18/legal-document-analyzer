2c1427a97cd698444eba7d45a8734344017f1b9d
Nikhil Jaiswar <nikhiljaiswar18@gmail.com>
2026-03-08 21:01:25 +0530

    Added PDF Upload API

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import shutil
+import os
 
 app = FastAPI()
+
+UPLOAD_FOLDER = "uploads"
 
 @app.get("/")
 def home():
     return {"message": "Legal Document Analyzer Backend Running"}
+
+
+@app.post("/upload")
+async def upload_document(file: UploadFile = File(...)):
+    
+    file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return {
+        "message": "File uploaded successfully",
+        "filename": file.filename
+    }
